fd356619a672bf7f75118a359a9d7642a9ccef26
Vishwa <vishwa56432@gmail.com>
2026-02-19 16:56:44 +0530

    cors

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ console.log("Starting server...");
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
 const app = express();
 const productroutes= require('./routes/productRoutes')
